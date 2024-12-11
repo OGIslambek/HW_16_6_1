3b60276924703af7039a0b456a6253ea06808b2f
OGislambek <ogislambek@gmail.com>
2024-12-11 22:13:37 +0300

    Добавьте файлы проекта.

---
+++ CalculatorTests/Calculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Addition_MustReturnCorrectValue()
+        {
+            int minValue = -1000;
+            int maxValue = 1000;
+            var calc = new Calculator();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                Assert.AreEqual(i + i, calc.Addition(i, i));
+                Assert.AreEqual(minValue + i + maxValue, calc.Addition(minValue, i, maxValue));
+            }
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            int minValue = -1000;
+            int maxValue = 1000;
+            var calc = new Calculator();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                Assert.AreEqual(maxValue - i, calc.Subtraction(maxValue, i));
+            }
+        }
+
+        [Test]
+        public void MultiplicationOfTwoAgrs_MustReturnCorrectValue()
+        {
+            int minValue = -1000;
+            int maxValue = 1000;
+            var calc = new Calculator();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                Assert.AreEqual(i * i, calc.Miltiplication(i, i));
+                Assert.AreEqual(minValue * i * maxValue, calc.Miltiplication(minValue, i, maxValue));
+            }
+        }
+
+        [Test]
+        public void MultiplicationOfTreeAgrs_MustReturnCorrectValue()
+        {
+            int minValue = -1000;
+            int maxValue = 1000;
+            var calc = new Calculator();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                Assert.AreEqual(minValue * i * maxValue, calc.Miltiplication(minValue, i, maxValue));
+            }
+        }
+
+        [Test]
+        public void Division_ByZeroMustThrowException()
+        {
+            var calc = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calc.Division(1000, 0));
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            int minValue = 1;
+            int maxValue = 1000;
+            var calc = new Calculator();
+            for (int i = minValue; i <= maxValue; i++)
+            {
+                Assert.AreEqual(maxValue / i, calc.Division(maxValue, i));
+            }
+        }
+    }
+}
---
+++ HW_16_6_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16_6_1
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Miltiplication(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
